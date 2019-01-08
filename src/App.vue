65780384f74cae7d52d2951fd36bcb78cba35691
Suguru Chinen <suguru.chinen@gmail.com>
2019-01-08 18:58:12 +0900

    start project with no design.

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Kasuya's 4:6 method</h1>
+    <h2>coffee</h2>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <ol>
+      <li>ペーパーを湯通しする</li>
+      <li>総湯量を40%と60%に分けて淹れる</li>
+    </ol>
+    <div>
+    豆の量（グラム）
+    <input type="text" v-model="beanAmount" />g
+    </div>
+    <div>
+      <p>{{ beanAmount }}</p>
+      <table class="table">
+        <thead>
+          <th>
+            累積時間
+          </th>
+          <th>
+            累積注湯量
+          </th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>0秒</td>
+            <td>{{ beanAmount * 2.5 }} ml</td>
+          </tr>
+          <tr>
+            <td>45秒</td>
+            <td>{{ beanAmount * 6 }} ml</td>
+          </tr>
+          <tr>
+            <td>1分30秒</td>
+            <td>{{ beanAmount * 9 }} ml</td>
+          </tr>
+          <tr>
+            <td>2分10秒</td>
+            <td>{{ beanAmount * 12 }} ml</td>
+          </tr>
+          <tr>
+            <td>2分45秒</td>
+            <td>{{ beanAmount * 15 }} ml</td>
+          </tr>
+          <tr>
+            <td>3分30秒</td>
+            <td>ドリッパーを外す</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <!-- <router-link to="/about">Go to About page.</router-link> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    beanAmount: 20
+  }),
+}
+</script>
